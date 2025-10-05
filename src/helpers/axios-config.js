1b9885f099a87b01837cd64fa3f68040e944a2b6
Juanosorno12 <esteban.osorno@est.iudigital.edu.co>
2025-10-05 16:51:40 -0500

    configurando baseURL para axios

--- src/helpers/axios-config.js
+++ src/helpers/axios-config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://movies-app-back.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
